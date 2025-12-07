d7b5d7dd3dd7690399320cd2d323273ea9b67980
Brian Bauska <brianbauska@gmail.com>
2025-12-06 18:40:35 -0800

    Update Confetti.js

--- js/Confetti.js
+++ js/Confetti.js
@@ -1,7 +1,7 @@
 const confettiContainer = document.querySelector('#confetti-container');
 const showConfetti = () => {
   const confetti = document.createElement('div');
-  confetti.textContent = '🥩';
+  confetti.textContent = '👻';
   confetti.classList.add('confetti');
   innerWidth = innerWidth * 0.5;
   confetti.style.left = Math.random() * innerWidth + 'px';
